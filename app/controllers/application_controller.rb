c991bf7ee95d288559aeb71b00949777a3095d61
Mandar Gokhale <mandar@letsalign.com>
2011-07-12 12:31:00 +0530

    changed password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
  
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == 'admin' && password == 'password'
+      user_name == 'admin' && password == 'pwd'
     end
   end
 end
